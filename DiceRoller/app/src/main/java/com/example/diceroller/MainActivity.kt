1a6c00ebae9bc7304d7d9b55e38161c5e4bad76d
shashidhar-gr <shashidhargr26@gmail.com>
2019-09-20 19:34:47 +0530

    Dice roller app

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rool_button)
+        val countButton: Button = findViewById(R.id.count_button)
+        rollButton.setOnClickListener { rollDice() }
+        countButton.setOnClickListener { count() }
+    }
+
+    private fun rollDice() {
+        val textView: TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(6) + 1;
+
+        textView.text = randomInt.toString();
+    }
+
+    private fun count() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        // If text is the default "Hello World!" set that text to 1.
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            // Otherwise, increment the number up to 6.
+            // The text value in resultText.text is an instance of the CharSequence class;
+            // it needs to be converted to a String object before it can be converted to an int.
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+    }
+}
