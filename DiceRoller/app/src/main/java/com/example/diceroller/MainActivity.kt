ce1a333c0a1dd8cbc1415b7b3baf2a9ff89f9da5
shashidhar-gr <shashidhargr26@gmail.com>
2019-09-20 23:25:24 +0530

    Roll dice app

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
@@ -10,46 +11,36 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage1 = findViewById(R.id.dice_image_1)
+        diceImage2 = findViewById(R.id.dice_image_2)
+
         val rollButton: Button = findViewById(R.id.rool_button)
-        val countButton: Button = findViewById(R.id.count_button)
-        val resetButton: Button = findViewById(R.id.reset_button)
         rollButton.setOnClickListener { rollDice() }
-        countButton.setOnClickListener { count() }
-        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
-        val textView: TextView = findViewById(R.id.result_text)
-        val randomInt = Random.nextInt(6) + 1;
-
-        textView.text = randomInt.toString();
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
     }
 
-    private fun count() {
-        val resultText: TextView = findViewById(R.id.result_text)
+    private fun getRandomDiceImage(): Int {
+        val randomInt = Random.nextInt(6) + 1;
 
-        // If text is the default "Hello World!" set that text to 1.
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            // Otherwise, increment the number up to 6.
-            // The text value in resultText.text is an instance of the CharSequence class;
-            // it needs to be converted to a String object before it can be converted to an int.
-            var resultInt = resultText.text.toString().toInt()
-
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
     }
 
-    private fun reset() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
-    }
 }
